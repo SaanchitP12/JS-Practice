6856cf5fefbb76324ecee78b0b86d447fff0c61a
prashantpaddune <padadunep@gmail.com>
2021-06-01 17:18:09 +0530

    variables lesson

---
+++ variables.js
@@ -0,0 +1,72 @@
+// console.log("Hello")
+
+
+// Variables 2 ways to declare
+// 1 - let varible_name
+// 2 - const variable_name
+// 3 - var outdated (same as let but has a very little difference,
+// will see that difference we come to that program.)
+
+
+/** forms to Declare variables **/
+// let name, age, lastname
+// name = 'sanchit';
+// age = '20';
+// lastname = 'pillai';
+
+// let name = 'sanchit', age = '20', lastname= 'pillai'
+
+// let name = 'hii my name is sanchit';
+// let age = 20;
+// let lastname= 'pillai';
+
+/** simple use of variables **/
+// let name = 'hii my name is sanchit'; // this is a name
+// let age = 20;     // this is age
+// let lastname= 'pillai'; // this is surname
+//
+// console.log('My name is ' + name + ' ' + lastname +', age is ' + age )
+
+/**
+ *  when to use let
+ *  values in let variable can be changed,
+ *  its not fixed
+ */
+// let name;
+// name = 'hii my name is sanchit'; // value declared first time.
+// name = "hii my name is prashant"; // new value declared.
+//
+// console.log(name) // this will print new value
+
+/**
+ * when to use const
+ * values in const variable is always fixed
+ * it can't be changed
+ */
+// const name // 0 or null or undefined
+// const name = 'prashant';
+//
+// console.log(name)
+
+
+/**
+ * working with let and const
+ */
+// let name;
+// name = 'sanchit';
+// console.log(name);
+//
+// const surname = 'paddune';
+// console.log(surname)
+
+
+/**
+ * TODO:
+ * 1 - Create a Variable name Fruits
+ * 2 - Fruits values should be in array eg: [sanchit, prashant]
+ * 3 - those values will be fixed to that variable
+ * 4 - print those values in console
+ *
+*/
+
+// Tomorrow TODO: DataTypes and Git and Github
