57d5cdb8b0f9d4f6bd8e6268f1c4d842b44bf778
SaanchitP12 <saanchitpillai.12@gmail.com>
2021-06-01 20:02:08 +0530

    Added variables and removed practice file

---
+++ saanchit/index.js
@@ -0,0 +1,20 @@
+
+console.log("Hello World")
+console.log(2+1)
+console.log("2"+1 ) 
+
+let fruits= ["orange", "mango", "banana", "apple"];
+console.log(fruits[2])
+
+let name = 'Saanchit';
+
+const job = 'Trader';
+console.log('My Name is ' + name + ' And I am a ' + job)
+
+name = 'Saanchit Pillai';
+console.log('My Name is ' + name)
+
+let age = 20;
+
+let car = {"color" : "red", "seats" : 6}
+console.log(car)
