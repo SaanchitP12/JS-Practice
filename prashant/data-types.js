785fd0684b2f68a53f76ca8efdf0f14ddbf89abf
prashantpaddune <padadunep@gmail.com>
2021-06-01 18:22:04 +0530

    lesson: data types

---
+++ prashant/data-types.js
@@ -0,0 +1,53 @@
+/*
+    Numbers
+ */
+const value = 2 + 5
+console.log('value is: ' + value + ', data type is: ' + typeof value)
+
+
+
+/*
+    bigInt
+ */
+const value1 = 2n + 555555555555555555555555555555555555555555555555555555555555555555555555555555555555n
+console.log('value is: ' + value1 + ', data type is: ' + typeof value1)
+
+
+
+/*
+    String
+
+    three types of quotes:
+    1 - double quotes
+    2 - single quotes
+    3 - back ticks
+ */
+
+const name = "Prashant"
+// back ticks usage
+const value2 = `hello : ${name} ` + `${value1}`
+console.log('value is: ' + value2 + ', data type is: ' + typeof value2)
+
+
+
+/*
+    boolean
+ */
+const value3 = 11 > 2
+console.log('value is: ' + value3 + ', data type is: ' + typeof value3)
+
+
+
+/*
+    null
+    it is not a data type but it defines empty value
+ */
+const value4 = null
+console.log('value is: ' + value4 + ', data type is: ' + typeof value4)
+
+
+/* undefined
+       variables are neither empty nor any other values
+ */
+
+// TODO : examples of all data types and push it to github
