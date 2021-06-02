8825570ae0a60e9eb6736b607038b8eb1e1d45b9
SaanchitP12 <saanchitpillai.12@gmail.com>
2021-06-02 12:19:31 +0530

    Added DataTypes

---
+++ saanchit/DataTypes.js
@@ -0,0 +1,35 @@
+/* String */
+const string ="Saanchit"
+console.log("My Name is " + string + ", datatype is " + typeof string)
+const string2 ='Student'
+console.log('I am a ' + string2 + ", datatype is " + typeof string2)
+const string3 =`${string} Pillai`
+console.log("My Full Name is " + string3 + ", datatype is " + typeof string3)
+
+/* Numbers */
+const number = 5 + 5
+const number1 = 5 * 5
+const number2 = 5 / 5
+const number3 = 5 - 5
+console.log("Value is " + number + ", datatype is " + typeof number)
+console.log("Value is " + number1 + ", datatype is " + typeof number1)
+console.log("Value is " + number2 + ", datatype is " + typeof number2)
+console.log("Value is " + number3 + ", datatype is " + typeof number3)
+
+/* Bigint */
+const bigint = 2222222222222222222222n + 22222222222222222222n
+console.log("Value is " + bigint + ", datatype is " + typeof bigint)
+const bigint1 = 44444444444444444444n - 44444444444444444444444n
+console.log("Value is " + bigint + ", datatype is " + typeof bigint)
+
+/* Boolean */
+const boolean = 453 > 350
+console.log("Value is " + boolean + ", datatype is " + typeof boolean)
+
+/* Null */
+const word = null
+console.log("Value is " + word + ", datatype is " + typeof word)
+
+/* Undefined */
+const nun
+console.log("Value is " + nun + ", datatype is " + typeof nun)
