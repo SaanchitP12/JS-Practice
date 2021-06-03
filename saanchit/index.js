a788cd0c0d7ffee85caa095de7d7fd92e4a055a4
SaanchitP12 <saanchitpillai.12@gmail.com>
2021-06-03 16:04:20 +0530

    Modified Index & Added Math functions

--- saanchit/index.js
+++ saanchit/index.js
@@ -7,8 +7,8 @@ console.log("2"+1 )
 /* To create a Variable there are 3 Types of Keywords
 1) let - with this keyword a variable can be created,
 and the value inside the variable can be changed again and again further in the codes 
-2) const -
-3) var -
+2) const - in this the value inside the variable cannot be changed in the future codes
+3) var - outdated (will see in further sessions)
 */
 
 let name = 'Saanchit';
@@ -23,12 +23,17 @@ let age = 20;
 console.log('My Age is ' + age)
 
 /* Array */
+/* In an Array, the values are written in "[]"(Square Brackets)
+and the values are designated position from 0, 1, 2, ......
+*/
 
 let fruits= ["orange", "mango", "banana", "apple"];
 console.log(fruits)
 console.log(fruits[2])
 
 /* Object */
-
+/* In an Object, the values are written in "{}"(Curly Bracket)
+in this variable we can give various informations as values
+*/
 let car = {"color" : "red", "seats" : 6}
 console.log(car)
--- saanchit/math.js
+++ saanchit/math.js
@@ -1,1 +1,59 @@
-/*Math Function*/
+/* Math Function */
+
+/* Math.abs */
+/* Math.abs function returns the absolute value of a number */
+console.log(Math.abs(15))
+console.log(Math.abs(-44))
+console.log(Math.abs('76'))
+console.log(Math.abs('-34'))
+console.log(Math.abs(null))
+
+/* Math.ceil */
+/* Math.ceil function rounds a number UP to the next larget integer */
+console.log(Math.ceil(6.93))
+console.log(Math.ceil(-6.93))
+console.log(Math.ceil(6.39))
+console.log(Math.ceil(-6.39))
+
+/* Math.floor */
+/* Math.floor function rounds a number DOWN to its nearest integer */
+console.log(Math.floor(7.29))
+console.log(Math.floor(-7.29))
+console.log(Math.floor(7.92))
+console.log(Math.floor(-7.92))
+
+/* Math.round */
+/* Math.round function rounds a number to its nearest integer */ 
+console.log(Math.round(18.45))
+console.log(Math.round(-18.45))
+console.log(Math.round(18.54))
+console.log(Math.round(-18.54))
+
+/* Math.trunc */
+/* Math.round function returns the integer part of a number */
+console.log(Math.trunc(20.83))
+console.log(Math.trunc(-90.13))
+console.log(Math.trunc(82.47))
+console.log(Math.trunc(-64.33))
+
+/* Math.max */
+/* Math.max function returns the number with the largest value */
+console.log(Math.max(12, 34, 56, 52))
+console.log(Math.max(-24, -53, -35, -12))
+
+const arr = [2, 34, 45,53 ,63 ,24]
+console.log(Math.max(...arr))
+
+/* Math.min */
+/* Math.min function returns the number with the lowest value */
+console.log(Math.min(12, 34, 56, 52))
+console.log(Math.min(-24, -53, -35, -12))
+
+const ran = [2, 34, 45, 53 , 63, 24]
+console.log(Math.min(...ran))
+
+/* Math.random */
+/* Math.random function returns random number between 0 to 1 */
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
