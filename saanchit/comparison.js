0638468df86f65c3bb8f112ad0be99ee335dcf98
SaanchitP12 <saanchitpillai.12@gmail.com>
2021-06-07 17:15:49 +0530

    added comparisons

---
+++ saanchit/comparison.js
@@ -0,0 +1,42 @@
+// TODO : 
+
+/* Basic Comparison Operators
+ <  less than
+ >  greater tham
+ == equal to
+ != not equal to 
+ <= less than equal to
+ >= greater than equal to */
+
+ /* Strict Comparison Operators 
+ === equal to 
+ !== not equal to */
+ 
+ /* Operators 
+  + Plus
+  - Minus
+  * Multiplication
+  / Divisiom
+  % Modulus */
+
+  console.log(784 == 5874)
+
+  console.log(251 > 54)
+
+  console.log(945 < 545)
+
+  console.log(null == undefined)
+
+  console.log(null === undefined)
+
+  console.log(null !== undefined)
+
+  console.log(null != undefined)
+
+  console.log(841 >= 'sss')
+
+  console.log(841 <= 'shs')
+
+  console.log(48 / 8)
+
+  console.log(48 % 8)
