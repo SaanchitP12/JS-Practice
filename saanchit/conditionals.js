2299d0f90f1a3b5b2b47276e85810beb680a71d4
prashantpaddune <padadunep@gmail.com>
2021-06-13 16:42:42 +0530

    update : saanchits assignment

--- saanchit/conditionals.js
+++ saanchit/conditionals.js
@@ -2,52 +2,25 @@
 /*  1 - There are 4 brands of cars eg: bmw, audi, fiat, honda,
         so if the brand names matches print that block with a string
         "This is a BRAND_NAME car" */
-     
-        let car = "BMW";
-        if  (car == "BMW") {
-            console.log("This is a BMW Car");
-        } else if (car == "Audi") {
-            console.log("This is a Audi Car");
-        } else if (car == "Fiat") {
-            console.log("This is a Fiat Car");
-        } else {
-            console.log("This is a Honda");
-        }
-        let car2 = "Audi";
-        if  (car2 == "BMW") {
-            console.log("This is a BMW Car");
-        } else if (car2 == "Audi") {
-            console.log("This is a Audi Car");
-        } else if (car2 == "Fiat") {
-            console.log("This is a Fiat Car");
-        } else {
-            console.log("This is a Honda");
-        }
-        let car3 = "Fiat";
-        if  (car3 == "BMW") {
-            console.log("This is a BMW Car");
-        } else if (car3 == "Audi") {
-            console.log("This is a Audi Car");
-        } else if (car3 == "Fiat") {
-            console.log("This is a Fiat Car");
-        } else {
-            console.log("This is a Honda");
-        }
-        let car4 = "Honda";
+
+        let car4 = "Audi";
+
         if  (car4 == "BMW") {
             console.log("This is a BMW Car");
         } else if (car4 == "Audi") {
             console.log("This is a Audi Car");
         } else if (car4 == "Fiat") {
             console.log("This is a Fiat Car");
-        } else { 
+        } else if (car4 == "Hondo"){
             console.log("This is a Honda Car");
+        } else {
+            console.log("No car found");
         }
 
- /*   2 - Sanchits age = 20
-        prashants age =  21
-        if the year is 2021 then addition the their ages else subtract it.
-        condition ? (age + age) : (age - age) */
+/*   2 - Sanchits age = 20
+       prashants age =  21
+       if the year is 2021 then addition the their ages else subtract it.
+       condition ? (age + age) : (age - age) */
 
         let year = 2021
         let age = year == 2021 ? (20 + 21) : (20 - 21)
